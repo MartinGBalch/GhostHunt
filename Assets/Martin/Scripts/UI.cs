98bf0515760fa4fd028534d55823a9bca5e61e1f
Martin Balch <s179122@students.aie.edu.au>
2017-09-29 12:31:45 -0700

    UI in polishing stage, timer done

---
+++ Assets/Martin/Scripts/UI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public SoulCarrier Carry;
+
+    Text carryCount;
+ 
+    public Color maxSoul;
+
+    public RawImage Counter;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        carryCount = GetComponent<Text>();
+        //Counter = GetComponent<RawImage>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		setSoulText ();
+    }
+
+    public void setSoulText()
+    {
+        carryCount.text = Carry.carriedSouls.ToString();
+        CounterColor();
+    }
+
+    void CounterColor()
+    {
+        if(Carry.carriedSouls >= 15)
+        {
+            Counter.color = maxSoul;
+            Debug.Log("working");
+        }
+    }
+
+   
+}
