9bec083dc8b3327ffd58440e7a071d3371aa9952
Martin Balch <s179122@students.aie.edu.au>
2017-09-28 17:54:09 -0700

    UI is functional but ugly

---
+++ Assets/Martin/UI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public SoulCarrier Carry;
+
+    Text carryCount;
+
+	// Use this for initialization
+	void Start ()
+    {
+        carryCount = GetComponent<Text>();
+      
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		setSoulText ();
+	}
+
+    void setSoulText()
+    {
+        carryCount.text = ": " + Carry.carriedSouls;
+    }
+}
