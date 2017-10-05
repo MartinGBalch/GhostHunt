1b5100f7b84df286ce45e87bdb4f1a643b1de84e
Kobey Myhre <s179139@students.aie.edu.au>
2017-10-05 11:40:00 -0700

    Controller EventSystem

---
+++ Assets/Kobey/UXinput/ControllerSelection.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+using UnityEngine.UI;
+public class ControllerSelection : MonoBehaviour
+{
+    public Button[] clickAbles;
+
+    PlayerIndex pIdx = PlayerIndex.One;
+    GamePadState state;
+    GamePadState prevState;
+    public int activeIndex;
+  
+    // Use this for initialization
+    void Start ()
+    {
+
+        for(int i =0; i < clickAbles.Length; i++)
+        {
+            
+            //clickAbles[i].gameObject.SetActive(false);
+        }
+        //activeIndex = 1;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        prevState = state;
+        state = GamePad.GetState(pIdx);
+
+        if(prevState.ThumbSticks.Left.Y == 0 && state.ThumbSticks.Left.Y > 0)
+        {
+            
+            activeIndex--;
+        }
+        if (prevState.ThumbSticks.Left.Y == 0 && state.ThumbSticks.Left.Y < 0)
+        {
+            
+            activeIndex++;
+        }
+
+        if (activeIndex == 3)
+        {
+            activeIndex = 0;
+        }
+        if (activeIndex == -1)
+        {
+            activeIndex = 2;
+        }
+        
+        if (prevState.Buttons.A == ButtonState.Released && state.Buttons.A == ButtonState.Pressed)
+        {
+            clickAbles[activeIndex].onClick.Invoke();
+        }
+        
+        clickAbles[activeIndex].Select();
+       
+    }
+}
+
+
+
+
