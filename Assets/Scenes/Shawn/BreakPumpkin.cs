8c1e8e2b08821b31583723255601bb6b3a1493c1
Shawn Hubbart <s179128@students.aie.edu.au>
2017-10-05 11:49:27 -0700

    Added destructible pumpkins to map prefab

---
+++ Assets/Scenes/Shawn/BreakPumpkin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakPumpkin : MonoBehaviour, Ikillable
+{
+    Animator anim;
+    Collider coll;
+    bool isAlive;
+	
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider>();
+	}
+
+    public void Die()
+    {
+        if (isAlive == true)
+        {
+            isAlive = false;
+            GetComponent<Collider>().enabled = false;
+            anim.SetBool("Break", true);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ghost" || other.gameObject.tag == "Test")
+            Physics.IgnoreCollision(coll, other.collider);
+    }
+}
