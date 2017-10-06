50db15f3a6c07ed019a4ffc48371434edd760141
Kobey Myhre <s179139@students.aie.edu.au>
2017-10-06 09:57:55 -0700

    MasterJuice

--- Assets/Kobey/LevelPreFabs/masterSoulDropOff.cs
+++ Assets/Kobey/LevelPreFabs/masterSoulDropOff.cs
@@ -9,6 +9,17 @@ public class masterSoulDropOff : MonoBehaviour {
     public int P3storedSouls;
     public int P4storedSouls;
 
+
+
+    public Light winLight;
+
+    public Color p1;
+    public Color p2;
+    public Color p3;
+    public Color p4;
+    Color Winner;
+
+
     public Timer time;
     public WinState[] winStates;
 
@@ -20,7 +31,26 @@ public class masterSoulDropOff : MonoBehaviour {
         P3storedSouls = 0;
         P4storedSouls = 0;
     }
-
+    void UpdateMasterLight()
+    {
+        int highScore = Mathf.Max(P1storedSouls, P2storedSouls, P3storedSouls, P4storedSouls);
+        if (P1storedSouls == highScore)
+        {
+            winLight.color = p1;
+        }
+        if (P2storedSouls == highScore)
+        {
+            winLight.color = p2;
+        }
+        if (P3storedSouls == highScore)
+        {
+            winLight.color = p3;
+        }
+        if (P4storedSouls == highScore)
+        {
+            winLight.color = p4;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Test")
@@ -54,8 +84,10 @@ public class masterSoulDropOff : MonoBehaviour {
 
             }
 
+            UpdateMasterLight();
         }
     }
+    
 
     void CalculateWinner()
     {
@@ -76,6 +108,9 @@ public class masterSoulDropOff : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        
+
+
         if(time.time < 0)
         {
             CalculateWinner();
