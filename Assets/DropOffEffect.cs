358bccd1d9f32987caa657bd9437eeb1441fa514
Kobey Myhre <s179139@students.aie.edu.au>
2017-10-27 15:26:53 -0700

    Added Shawns DropOffEffect script

---
+++ Assets/DropOffEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropOffEffect : MonoBehaviour {
+
+    // Use this for initialization
+    float timer = 2;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            timer = 2;
+            gameObject.SetActive(false);
+        }	
+	}
+}
